1fa168ac5304b6eed4c929870800fe62b4ac794e
Vito Masciulli <sober83@gmail.com>
2019-01-07 20:34:29 +0100

    added acc loss plot visual

--- MNIST-LeNet-5.py
+++ MNIST-LeNet-5.py
@@ -1,10 +1,11 @@
+from __future__ import print_function
 import keras
+import matplotlib.pyplot as plt
 from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
 from keras.layers import Flatten,  MaxPooling2D, Conv2D
 from keras.callbacks import TensorBoard
-
 #Leggi e processa i dati del dataset MNIST
 (X_train,y_train), (X_test, y_test) = mnist.load_data()
 X_train = X_train.reshape(60000,28,28,1).astype('float32')
@@ -33,8 +34,25 @@ model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accur
 tensor_board = TensorBoard('./logs/MNIST-LeNet-5')
 
 #Apprendimento modello
-model.fit(X_train, y_train, batch_size=128, epochs=15, verbose=1,
-validation_data=(X_test,y_test), callbacks=[tensor_board])
+#history = model.fit(X_train, y_train, batch_size=128, epochs=15, verbose=1, validation_data=(X_test,y_test))
+history = model.fit(X_train, y_train, batch_size=128, epochs=15, verbose=1, validation_data=(X_test,y_test), callbacks=[tensor_board])
+# Plot training & validation accuracy values
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('Model accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.show()
+
+# Plot training & validation loss values
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Model loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.show()
 
 #Invia "tensorboard --logdir=./logs --port 6006" su terminale per analisi visiva su TensorBoard
 
