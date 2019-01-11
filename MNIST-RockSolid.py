822e38b5bfad1b38ec61e1d5ff0149d0aba485ad
Vito Masciulli <sober83@gmail.com>
2019-01-11 21:44:19 +0100

    Added y_test Confusion Matrix plot

--- MNIST-RockSolid.py
+++ MNIST-RockSolid.py
@@ -1,5 +1,8 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from sklearn.metrics import confusion_matrix
 
 # Import MNIST Dataset
 (x_train, y_train),(x_test, y_test) = tf.keras.datasets.mnist.load_data()
@@ -44,18 +47,30 @@ tf.keras.models.load_model (Nomefile)
 print(Nomefile,' Model saved')
 V_loss, V_acc = model.evaluate(x_test, y_test)
 print('RockSolid [',Nomefile, '] accuracy=[', V_acc*100, "%]   loss=[", V_loss,"]")
-plt.subplot(1,2,1)
+
+plt.figure(figsize=(40, 20))
+
+plt.subplot(2,2,1)
 plt.title('Accuracy ' + Nomefile)
 plt.plot(history.history['acc'])
 plt.plot(history.history['val_acc'])
 plt.ylabel('Accuracy')
 plt.xlabel('Epoch')
-plt.legend(['Train', 'Test'], loc='upper left')
-plt.subplot(1,2,2)
+plt.legend(['Train', 'Test'], loc='lower right')
+
+plt.subplot(2,2,2)
 plt.title('Loss ' + Nomefile)
 plt.plot(history.history['loss'])
 plt.plot(history.history['val_loss'])
 plt.ylabel('Loss')
 plt.xlabel('Epoch')
-plt.legend(['Train', 'Test'], loc='upper left')
+plt.legend(['Train', 'Test'], loc='upper right')
+
+plt.subplot(2,2,3)
+plt.title('y_test Confusion Matrix')
+label = np.argmax(model.predict(x_test), axis=1)
+target = np.argmax(y_test, axis=1)
+confmat = confusion_matrix(target, label)
+sns.heatmap(confmat, annot=True, cmap='tab20c',fmt='d',linewidths=.5)
+
 plt.show()
