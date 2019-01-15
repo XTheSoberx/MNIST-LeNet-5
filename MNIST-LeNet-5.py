a058bff9a4605e400eecb1565ef6a63f23940167
Vito Masciulli <sober83@gmail.com>
2019-01-15 19:27:37 +0100

    only use Tensorlow module

--- MNIST-LeNet-5.py
+++ MNIST-LeNet-5.py
@@ -1,11 +1,10 @@
 from __future__ import print_function
-import keras
+import tensorflow as tf
 import matplotlib.pyplot as plt
-from keras.datasets import mnist
-from keras.models import Sequential
-from keras.layers import Dense, Dropout
-from keras.layers import Flatten,  MaxPooling2D, Conv2D
-from keras.callbacks import TensorBoard
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Conv2D, Flatten, MaxPooling2D
+
 
 #Leggi e processa i dati del dataset MNIST
 (X_train,y_train), (X_test, y_test) = mnist.load_data()
@@ -14,8 +13,8 @@ X_test = X_test.reshape(10000,28,28,1).astype('float32')
 X_train /= 255
 X_test /= 255
 n_classes = 10
-y_train = keras.utils.to_categorical(y_train, n_classes)
-y_test = keras.utils.to_categorical(y_test, n_classes)
+y_train = tf.keras.utils.to_categorical(y_train, n_classes)
+y_test = tf.keras.utils.to_categorical(y_test, n_classes)
 
 #Crea architettura rete neurale LeNet-5
 model = Sequential()
@@ -32,7 +31,7 @@ model.add(Dense(n_classes, activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 #Imposta i dati log per analisi visiva su TensorBoard
-tensor_board = TensorBoard('./logs/MNIST-LeNet-5')
+tensor_board = tf.keras.callbacks.TensorBoard('./logs/MNIST-LeNet-5')
 
 #Apprendimento modello
 history = model.fit(X_train, y_train, batch_size=128, epochs=15, verbose=1, validation_data=(X_test,y_test), callbacks=[tensor_board])
@@ -45,7 +44,7 @@ print('[This model  accuracy=[', V_acc*100, "%]   loss=[", V_loss,"]]")
 Nomefile = input('inserire il nome del file dove verrà salvato il modello ')
 Nomefile = Nomefile + '.h5'
 model.save (Nomefile)
-keras.models.load_model (Nomefile)
+tf.keras.models.load_model (Nomefile)
 print(Nomefile,' salvato correttamente')
 print('modello [',Nomefile, '] accuracy=[', V_acc*100, "%]   loss=[", V_loss,"]")
 
